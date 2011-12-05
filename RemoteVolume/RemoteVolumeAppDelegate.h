6e678e429e77354e21ff0cc20e65301b79fd5822
Christopher Rizzo <c@crizzo.me>
2011-12-05 10:54:08 -0500

    Simple Desktop volume controller for Mac.
    Basic layout. Unfinished.

--- RemoteVolume/RemoteVolumeAppDelegate.h
+++ RemoteVolume/RemoteVolumeAppDelegate.h
@@ -11,5 +11,10 @@
 @interface RemoteVolumeAppDelegate : NSObject <NSApplicationDelegate>
 
 @property (assign) IBOutlet NSWindow *window;
+@property (weak) IBOutlet NSTextField *textField;
+@property (weak) IBOutlet NSSlider *slider;
 
+
+- (IBAction)takeFloatValueForVolumeFrom:(id)sender;
+- (IBAction)mute:(id)sender;
 @end
--- RemoteVolume/RemoteVolumeAppDelegate.m
+++ RemoteVolume/RemoteVolumeAppDelegate.m
@@ -11,10 +11,27 @@
 @implementation RemoteVolumeAppDelegate
 
 @synthesize window = _window;
+@synthesize textField = _textField;
+@synthesize slider = _slider;
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
 }
 
+- (IBAction)mute:(id)sender {
+    NSLog(@ "recieved a mute message");
+}
+
+- (IBAction)takeFloatValueForVolumeFrom:(id)sender {
+    
+    NSString *senderName = nil;
+    if (sender == _textField) {
+        senderName = @"textField";
+    }
+    else {
+        senderName = @"slider";
+    }
+    NSLog(@"%@ sent takeFloatValueForVolumeFrom: with value %1.2f", senderName, [sender floatValue]);
+}
 @end
